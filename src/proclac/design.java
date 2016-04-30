dd14b0a080d2372ac592175968ab04cce8d9912d
calculator <hendsalaheldeen14@gmail.com>
2016-05-01 01:30:12 +0200

    design of the numbers and their events

--- src/proclac/design.java
+++ src/proclac/design.java
@@ -1,5 +1,98 @@
 package proclac;
+import javax.script.*;
 
-public class design {
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class design  extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(args);
+
+	}
+	Button [] arr = new Button[10];
+	TextField text = new TextField();
+	String res="";
+	 static int size=0;   ///the size of the new array according to the expression
+	@Override
+	public void start(Stage st) throws Exception {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+text.setPrefHeight(70);
+text.setPrefWidth(300);
+GridPane g = new  GridPane();
+g.add(text,0,0,6,2);	
+
+//buttoms from 1 to 9
+
+
+
+for(int i =1; i<10 ;i++){
+arr[i]= new Button(Integer.toString(i));
+
+arr[i].setPrefSize(50,40);
 
 }
+int k=9;
+for(int i =2 ;i<=4 ;i++)
+for(int j=2;j>=0;j--)
+g.add(arr[k--], j, i);
+
+/////////////////////////////////////////////////////////////////////////////////
+g.setHgap(3);
+g.setVgap(3);		
+st.setScene(new Scene(g,300,200));
+st.setResizable(false);
+
+st.show();
+/////////////////////////////////////////////////////////////////////////////////
+// the event of the numbers from 1 to 9 
+arr[1].setOnAction(e->{
+text.setText(text.getText()+arr[1].getText());
+});
+
+arr[2].setOnAction(e->{
+text.setText((text.getText()+arr[2].getText()));
+});
+
+
+arr[3].setOnAction(e->{
+text.setText((text.getText()+arr[3].getText()));
+});
+
+arr[4].setOnAction(e->{
+text.setText((text.getText()+arr[4].getText()));
+});
+
+arr[5].setOnAction(e->{
+text.setText((text.getText()+arr[5].getText()));
+});
+
+arr[6].setOnAction(e->{
+text.setText((text.getText()+arr[6].getText()));
+});
+
+arr[7].setOnAction(e->{
+text.setText((text.getText()+arr[7].getText()));
+});
+
+arr[8].setOnAction(e->{
+text.setText((text.getText()+arr[8].getText()));
+});
+
+arr[9].setOnAction(e->{
+text.setText((text.getText()+arr[9].getText()));
+
+});
+
+	}}
+	
+	
